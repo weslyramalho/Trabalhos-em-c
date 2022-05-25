449e8dfdedfcb3bcbbed727c2d1ff6794c477deb
Wesly <weslyramalho@yahoo.com.br>
2022-05-25 19:06:11 -0300

    AgenciaBancaria

---
+++ AgenciaBancaria.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void main(void){
+	unsigned numConta, verificador;
+	
+	printf("Digite o numero da conta com ate 6 digitos: ");
+	scanf("%u", &numConta);
+	
+	printf("A %u", numConta);
+	
+	
+}
