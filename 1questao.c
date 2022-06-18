49b1209409753e38fd2ee56e39508d1e3d6e9bcc
Wesly <weslyramalho@yahoo.com.br>
2022-06-17 21:34:13 -0300

    atividade 1 final

--- 1questao.c
+++ 1questao.c
@@ -1,35 +1,56 @@
-/*
-WESLY RAMALHO LOPES
-1 -  Questão
-*/
-
-#include<stdio.h>
-#include <locale.h>
+#include <stdio.h>
 
 void main(void){
-	setlocale(LC_ALL, "Portuguese");
-	int n,i,a=0,totala=0;
-	
-	printf("Entre com o valor de n: ");
+	int i, j,n;
+
+	printf("Digite o valor de 'N': ");
 	scanf("%d", &n);
-	int vet[n];
+	float a[n][n], ta[n][n], ra[n][n];
+
+	for (i = 0; i < n; i++){
+	for (j = 0; j < n; j++) {
+	printf("Entre com dado da linha %d e coluna %d: ", i, j);
+	scanf("%f", &a[i][j]);
+}
+}
+	printf("\nMatiz 'A'\n");
+	for (i = 0; i < n; i++) {
+	for (j = 0; j < n; j++)
+	printf("[%0.1f], ", a[i][j]);
+	printf("\n");
+}	
+	for (i = 0; i < n; i++) {
+	for (j = 0; j < n; j++)
+	ta[j][i]=a[j][i];
+}
 	
-	printf("Digite o valor de 'a':");
-	scanf("%d", &a);
+	printf("\nMatriz transposta\n");
+	for (i = 0; i < n; i++) {
+	for (j = 0; j < n; j++)	
+	printf("[%0.1f], ", ta[j][i]);
+	printf("\n");
+
+}	
+	for(i=0; i < n; i++){
+	for(j=0; j < n; j++)
+	if(i+j+2==n+1)ra[j][i]= a[i][j]+ ta[j][i];
+	}
 	
 	for(i=0; i < n; i++){
-		printf("Digite o valor do elemento na posição %d :", i);
-		scanf("%d", &vet[i]);
-	}
-	for(i=0; i < n; i++){
-		if(a == vet[i]) {
-		totala++;
-	}
-		printf("\nO valor do elemento na possição %d é %d", i, vet[i]);	 
+	for(j=0; j < n; j++)
+	if(i+j+2!=n+1) ra[j][i]=ta[j][i];
+}
+	
+
+	printf("\nMatriz RA\n");
+	for(i = 0; i < n; i++){
+	for(j = 0; j < n; j++)
+	printf("[%0.1f], ", ra[j][i]);
+	printf("\n");
+
 	}
 		
-	printf("\nO valor de 'a' é:  %d ", a);
-	printf("\nO valor %d aparece %d vezes: ", a, totala);
-	
 }
+
+	
 
